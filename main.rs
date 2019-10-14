ed2f13be7c9238908ed256766a8ffe6b6f9ebace
qq0827 <dex827@hotmail.com>
2019-10-14 22:54:29 +0800

    add function or method or varable scope

--- main.rs
+++ main.rs
@@ -1,10 +1,10 @@
-use std::io;
 use std::collections::HashMap;
+use std::io;
 use std::iter::Map;
-use std::panic::resume_unwind;
+use std::time;
+
 
 fn main() {
-
     base_type();
 
     char_type();
@@ -20,6 +20,15 @@ fn main() {
     enum_test();
 
     expression_test();
+
+    scope_test();
+
+    func_test();
+    func_call();
+
+    callback_call();
+
+    method_call();
 }
 
 ///# 基本类型
@@ -30,47 +39,47 @@ fn main() {
 /// ```
 fn base_type() {
     //基本类型
-    let flag: bool = true;      //bool只有两个值true和false
+    let flag: bool = true; //bool只有两个值true和false
 
     //无符号整型
-    let x1: u8 = 56;            //u8类型,占用1个字节
-    let x2: u16 = 110;          //u16类型,占用2个字节
-    let x3: u32 = 1000;         //u32类型,占用4个字节
-    let x4: u64 = 10000000;     //u64类型,占用8个字节
-    let x5: u128 = 100000000;   //u128类型,占用16个字节
+    let x1: u8 = 56; //u8类型,占用1个字节
+    let x2: u16 = 110; //u16类型,占用2个字节
+    let x3: u32 = 1000; //u32类型,占用4个字节
+    let x4: u64 = 10000000; //u64类型,占用8个字节
+    let x5: u128 = 100000000; //u128类型,占用16个字节
 
     //符号整数
-    let x6: i8 = -56;             //i8类型,占用1个字节
-    let x7: i16 = 110;            //i16类型,占用2个字节
-    let x8: i32 = 1000;           //i32类型,占用4个字节
-    let x9: i64 = 10000000;       //i64类型,占用8个字节
-    let x10: i128 = 100000000;    //i128类型,占用16个字节
+    let x6: i8 = -56; //i8类型,占用1个字节
+    let x7: i16 = 110; //i16类型,占用2个字节
+    let x8: i32 = 1000; //i32类型,占用4个字节
+    let x9: i64 = 10000000; //i64类型,占用8个字节
+    let x10: i128 = 100000000; //i128类型,占用16个字节
 
     //浮点数类型分f32和f64
-    let f1: f32 = 3223.22;   //单精度位浮点数
-    let f2: f64 = 10.0;     //双精度浮点数
-    let f3 = 10.0;    //f3变量,类型推导为f64双精度浮点数
+    let f1: f32 = 3223.22; //单精度位浮点数
+    let f2: f64 = 10.0; //双精度浮点数
+    let f3 = 10.0; //f3变量,类型推导为f64双精度浮点数
 }
 
 //字符类型
 fn char_type() {
-    let c1 = 'a';     //定义字符类型
-    let c2 = '\'';    //字符转义其他语言一样,用\,两个\\输出一个\, 单引号出输出\'
+    let c1 = 'a'; //定义字符类型
+    let c2 = '\''; //字符转义其他语言一样,用\,两个\\输出一个\, 单引号出输出\'
     println!("{}", c1);
     println!("{}", c2);
-    println!("c1 as u8={}", c1 as u8);  //通过as进行类型转换，将字符转为 u8类型
+    println!("c1 as u8={}", c1 as u8); //通过as进行类型转换，将字符转为 u8类型
 }
 
 fn compound_type() {
     //数组
-    let arr1: [i32; 3] = [0, 1, 2];      //声明一个长度为3的i32类型数组,这个用法稍微有点怪
-    let arr2 = [0, 1, 2];        //一般都使用简写
+    let arr1: [i32; 3] = [0, 1, 2]; //声明一个长度为3的i32类型数组,这个用法稍微有点怪
+    let arr2 = [0, 1, 2]; //一般都使用简写
 
-    let last_val = arr1[2];        //访问数组下标为2的值
+    let last_val = arr1[2]; //访问数组下标为2的值
     println!("array arr[2]={}", last_val);
 
     //元组,在rust中是原生的类型,和csharp(c#)不一样
-    let xx = (12, "hello rust", 110.1);  //声明一个类型有i32类型,字符串类型,f64类型的元组
+    let xx = (12, "hello rust", 110.1); //声明一个类型有i32类型,字符串类型,f64类型的元组
     //使用下标获取元组的元素
     println!("index=0 val={}", xx.0);
     println!("index=1 val={}", xx.1);
@@ -84,13 +93,12 @@ fn compound_type() {
 }
 
 fn range_type() {
-    let x = std::ops::Range { start: 1, end: 10 };  //声明一个Range左闭右开区间范围类型
-    let y = (1..10);                    //左闭右开区间简写的方式
+    let x = std::ops::Range { start: 1, end: 10 }; //声明一个Range左闭右开区间范围类型
+    let y = (1..10); //左闭右开区间简写的方式
     assert_eq!(x, y);
 
-
-    let x2 = std::ops::RangeInclusive::new(1, 10);  //全闭区间范围类型
-    let y2 = (1..=10);                                        //全闭区间简写方式
+    let x2 = std::ops::RangeInclusive::new(1, 10); //全闭区间范围类型
+    let y2 = (1..=10); //全闭区间简写方式
     assert_eq!(x2, y2);
 }
 
@@ -98,9 +106,9 @@ fn slice_type() {
     let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
     //切片类型 通过引用符&对数组引用,产生切片,进行范围操作
-    let lists = &arr[0..5];       //切片类型通过数组地址获取数组下标为0开始,5个元素,中间不产生新的数组
+    let lists = &arr[0..5]; //切片类型通过数组地址获取数组下标为0开始,5个元素,中间不产生新的数组
 
-    assert_eq!(lists.len(), 5);           //验证lists的长度是否为5
+    assert_eq!(lists.len(), 5); //验证lists的长度是否为5
 
     //遍历lists的元素,和arr前五个元素进行对比
     for item in lists {
@@ -152,77 +160,141 @@ fn map_test() {
 }
 
 fn expression_test() {
-//if语句和其他语言基本一样，只是没有条件没有小括号
-let x = 100;
-if x > 0 {
-    println!("x >0");
-} else {
-    println!("x < 0")
-}
-
-//多重if语句
-let y = 200;
-if y > 10 {
-    println!("level 1");
-} else if y > 20 {
-    println!("level 2");
-} else if y > 30 {
-    println!("level 3");
-} else {
-    println!("level 5");
-}
-
-//let和if组合使用
-let num = if y > 100 {
-    1
-} else {
-    0
-};
-println!("num = {}", num);
-
-//loop 死循环,相当于其他语言的 while(true){ //执行代码} 或者 for(;;){ //执行代码 }
-//下面代码进行注释，不然下面的代码无法执行
-////    loop {
-////        println!("loop out ....");
-////    }
-
-//loop和let组合使用
-let mut counter = 0;
-let result = loop {
-    counter += 1;
-    if counter == 10 {
-        break counter * 2;
+    //if语句和其他语言基本一样，只是没有条件没有小括号
+    let x = 100;
+    if x > 0 {
+        println!("x >0");
+    } else {
+        println!("x < 0")
     }
-};
-println!("result = {}", result);
 
-//while
-let mut a = 0;
-while a < 10 {
-    a += 1;
-    println!("a = {}", a);
+    //多重if语句
+    let y = 200;
+    if y > 10 {
+        println!("level 1");
+    } else if y > 20 {
+        println!("level 2");
+    } else if y > 30 {
+        println!("level 3");
+    } else {
+        println!("level 5");
+    }
+
+    //let和if组合使用
+    let num = if y > 100 { 1 } else { 0 };
+    println!("num = {}", num);
+
+    //loop 死循环,相当于其他语言的 while(true){ //执行代码} 或者 for(;;){ //执行代码 }
+    //下面代码进行注释，不然下面的代码无法执行
+    ////    loop {
+    ////        println!("loop out ....");
+    ////    }
+
+    //loop和let组合使用
+    let mut counter = 0;
+    let result = loop {
+        counter += 1;
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+    println!("result = {}", result);
+
+    //while
+    let mut a = 0;
+    while a < 10 {
+        a += 1;
+        println!("a = {}", a);
+    }
+
+    //for
+    for num in (1..100) {
+        println!("for inc num = {}", num);
+    }
+
+    //match 在rust语言中，没有switch关键字,可以通过match实现相同的功能
+    //_ 相当于其他语言中default
+    let level = 2;
+    match level {
+        1 => println!("level 1"),
+        2 => println!("level 2"),
+        3 => println!("level 3"),
+        4 => println!("level 4"),
+        _ => println!("level 5"),
+    }
 }
 
-//for
-for num in (1..100) {
-    println!("for inc num = {}", num);
+///# 函数的使用
+/// 1. fn 后跟函数名+小括号  ->()相当于其他语言中void表示无返回值,可以简化不写
+/// 2. { //函数主体内容 }
+/// 简写方式 fn func_test() { rintln!("denfine function!"); }
+fn func_test() -> () {
+    println!("denfine function!");
 }
 
-//match 在rust语言中，没有switch关键字,可以通过match实现相同的功能
-//_ 相当于其他语言中default
-let level = 2;
-match level {
-    1 => { println!("level 1") }
-    2 => println!("level 2"),
-    3 => println!("level 3"),
-    4 => println!("level 4"),
-    _ => println!("level 5")
+///# 带返回值的函数
+///1. 在函数名后括号中 先写参数名 后跟参数类型. 这和c/c++/c#不同, 和go语言一样
+///2. 和其他语言(c/c++/c#/go)不一样, 返回值前不用加return,这点有点怪异,在业务逻辑复杂的情况,看代码不是很好
+fn add(x: i32, y: i32) -> i32 {
+    x + y
 }
+
+///# 调用带返回值的add函数
+fn func_call() {
+    let x = 100;
+    let y = 200;
+    let result = add(x, y);
+    println!("x+y={}", result);
+}
+
+///# hi函数作为参数调用
+fn hi(name: &str) {
+    println!("call {} () function.", name);
+}
+
+///# hello函数最为参数调用
+fn hello(name: &str) {
+    println!("call {} () function.", name);
+}
+
+///# 参数为函数指针
+///可以这样理解func:fn(&str)  => fn func(&str) &str为字符串
+fn callback_(name: &str, func: fn(&str)) {
+    func(name);
+}
+
+///# 回调函数使用
+fn callback_call() {
+    let str1 = "hi";
+    let str2 = "hello";
+    //第二个参数传入hi函数
+    callback_(str1, hi);
+    //第二个参数传入hello函数
+    callback_(str2, hello);
+}
+
+///#
+fn method_call() {
+    let p = People {
+        id: 1,
+        name: "tom",
+        sex: "man",
+    };
+    p.show();  //调用p中show方法
+}
+
+///# 作用域
+///
+fn scope_test() {
+    //x的作用域是在函数内
+    let x = 10;
+    {
+        //y的作用域在 所在的大括号内
+        let y = 20;
+    }
 }
 
 fn main1() {
-
-
     //条件分支
     let x = 6;
     if x == 5 {
@@ -230,10 +302,10 @@ fn main1() {
     }
     //match rust没有switch关键字，这里相当于switch，不过更简洁
     match x {
-        1 => { println!("one") }
+        1 => println!("one"),
         2 => println!("two"),
         5 => println!("five"),
-        _ => println!("default")
+        _ => println!("default"),
     }
 
     //for循环
@@ -255,23 +327,12 @@ fn main1() {
     println!("pow 3 of 10 is {}.", p3);
 }
 
-fn hi(name: &str) {
-    println!("hi,{}.", name);
-}
-
-fn hello(name: &str) {
-    println!("hello,{}", name);
-}
 
 //回调函数
 fn say_what(name: &str, func: fn(&str)) {
     func(name);
 }
 
-///带返回值的函数
-fn add(x: i32, y: i32) -> i32 {
-    x + y
-}
 
 ///多返回值
 fn pow_2_3(n: i32) -> (i32, i32) {
@@ -286,7 +347,15 @@ struct People {
     //id
     name: &'static str,
     //姓名 字符串
-    sex: &'static str,  //性别
+    sex: &'static str, //性别
+}
+
+///# 使用impl对对象People增加行为(定义方法)
+impl People {
+    //self 表示自身(这里为People)
+    fn show(&self) {
+        println!("id={} name={}", self.id, self.name);
+    }
 }
 
 #[derive(Debug)]
@@ -295,5 +364,5 @@ enum WindowState {
     //最小化
     Normal,
     //正常
-    Max,       //最大化
+    Max, //最大化
 }
